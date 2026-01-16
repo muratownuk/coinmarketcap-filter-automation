a4967e0af27c0719fd27a7d85955b3b1493bae2a
Murat Ownuk <ilmuratownuk@gmail.com>
2026-01-15 20:20:51 -0500

    updated write instead of press for pygui for efficiency. Also increased the amount of press times for better response with browser

--- coinmarketcap_filter_func.py
+++ coinmarketcap_filter_func.py
@@ -1,44 +1,43 @@
 import pyautogui, time 
 from filter_defines import *
 
-SLEEP_TIME_BETWEEN_PRESS=0.5
+SLEEP_TIME_BETWEEN_PRESS_FAST=0.5
+SLEEP_TIME_BETWEEN_PRESS_MID=1
+SLEEP_TIME_BETWEEN_PRESS_SLOW=2
 
 ## Functions ## 
 def filter_startup():
 
      # essential filter start-up #
     pyautogui.click(FiltersButton.x, FiltersButton.y)
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     pyautogui.click(VisibleCoinsButton.x, VisibleCoinsButton.y)
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     pyautogui.click(ShowAllButton.x, ShowAllButton.y)
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     # crypto.com is the exchange we are focused on as that is our exchange. 
     pyautogui.click(ExchangeButton.x, ExchangeButton.y)
-    pyautogui.press("c")
-    pyautogui.press("r")
-    pyautogui.press("y")
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    pyautogui.write("cry", interval=SLEEP_TIME_BETWEEN_PRESS_FAST)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     pyautogui.click(CryptoComExchangeButton.x, CryptoComExchangeButton.y)
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     # filter out low 24h volume coins (rug-pull potential)
     pyautogui.click(Volume24hMinButton.x, Volume24hMinButton.y)
-    pyautogui.press("1")
-    pyautogui.press("0")
-    pyautogui.press("0")
-    pyautogui.press("0")
-    pyautogui.press("0")
-    pyautogui.press("0")
-    pyautogui.press("0")
-    time.sleep(SLEEP_TIME_BETWEEN_PRESS)
+    pyautogui.write("1000000", interval=SLEEP_TIME_BETWEEN_PRESS_FAST)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_FAST)
 
     pyautogui.click(ApplyButton.x, ApplyButton.y)  
 
-
+def OneHrPrcntRefresh():
+    pyautogui.click(OneHrPrcntButton.x, OneHrPrcntButton.y)
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_MID)
+    pyautogui.click(OneHrPrcntButton.x, OneHrPrcntButton.y) 
+    time.sleep(SLEEP_TIME_BETWEEN_PRESS_MID) 
+    pyautogui.click(OneHrPrcntButton.x, OneHrPrcntButton.y) 
 
 
